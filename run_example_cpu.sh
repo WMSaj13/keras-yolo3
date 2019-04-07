01ce0289c87f28f7b9c035800f1e2ac4dcd5d0be
Władysław Saj <wladyslaw.saj@gmail.com>
2019-04-07 20:11:47 +0200

    fix gpu_num not num_gpu

--- run_example_cpu.sh
+++ run_example_cpu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 yolo_video.py --model_path model_data/yolo.h5 --classes_path model_data/coco_classes.txt --num_gpu 0 --input ../example.mp4
+python3 yolo_video.py --model_path model_data/yolo.h5 --classes_path model_data/coco_classes.txt --gpu_num 0 --input ../example.mp4
